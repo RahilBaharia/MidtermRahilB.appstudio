944b27d5f172df4dd546e29cd51a5eb3fe7f08bc
RahilBaharia <79479601+RahilBaharia@users.noreply.github.com>
2021-03-15 14:17:29 -0500

    Add animals to an array and formatted lowercase

---
+++ forms/animals/animals.js
@@ -0,0 +1,7 @@
+let animals = ["dog", "cat", "horse", "meerkat"]
+
+let diffAnimal = prompt("To add to the array: Enter an Animal")
+let lastAnimal = diffAnimal.toLowerCase()
+
+animals.push(lastAnimal)
+alert(`The last animal is ${animals[animals.length - 1]}`)
